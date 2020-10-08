1373980536f22ff185e4ddb5565c4d8dad917a6e
temoji <teeeemoji@163.com>
2020-10-08 11:36:52 +0800

    fix: fix website title & favico

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,12 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     outputDir: 'docs',
     pages: {
-        index: 'website/main.js'
+        index: {
+            entry: 'website/main.js',
+            title: 'Front End Interview Questions',
+            favicon: './website/favicon.ico',
+            template: './website/template.html'
+        }
     },
     chainWebpack: config => {
         // questions 目录下的资源, 使用自定义 loader 进行解析 (将 ts 源码解析成我们需要的格式)
